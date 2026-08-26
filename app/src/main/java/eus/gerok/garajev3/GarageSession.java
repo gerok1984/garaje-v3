837abe2cac306402da495aca8b76de7facf963be
gerok1984 <83238357+gerok1984@users.noreply.github.com>
2026-08-26 22:25:53 +0200

    Añadir sesión Android Auto

---
+++ app/src/main/java/eus/gerok/garajev3/GarageSession.java
@@ -0,0 +1,15 @@
+package eus.gerok.garajev3;
+
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.car.app.Screen;
+import androidx.car.app.Session;
+
+public class GarageSession extends Session {
+    @NonNull
+    @Override
+    public Screen onCreateScreen(@NonNull Intent intent) {
+        return new GarageScreen(getCarContext());
+    }
+}
